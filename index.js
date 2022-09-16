fe2f5a4bbf779d40f44b711bafea890e1ddf3b1b
Josi <jtk.jpaf@gmail.com>
2022-09-16 09:21:47 -0400

    middleware

--- index.js
+++ index.js
@@ -13,34 +13,42 @@ server.listen(3000, () => {
 
 const express = require("express");
 const app = express();
-app.use(express.json());
 
-app.all('/user', (req, res,next) => {
-  console.log('por aqui paso');
-  next();
+function logger(req, res, next) {
+    console.log("Request received");
+    next();
+}
+
+app.use(express.json());
+app.use(logger);
+
+app.all("/user", (req, res, next) => {
+    console.log("por aqui paso");
+    next();
 });
 
 app.get("/user", (req, res) => {
-  res.json({
-    username: "Cameron",
-    latname: "Boice",
-  });
+    res.json({username: "Cameron", latname: "Boice"});
 });
 
 app.post("/user/:id", (req, res) => {
-  console.log(req.body);
-  console.log(req.params);
-  res.send("PETICION POST");
+    console.log(req.body);
+    console.log(req.params);
+    res.send("PETICION POST");
 });
 app.put("/user/:userId", (req, res) => {
-  console.log(req.body);
-  res.send(`User  ${req.params.userId} update`);
+    console.log(req.body);
+    res.send(`User  ${
+        req.params.userId
+    } update`);
 });
 
 app.delete("/user/:id", (req, res) => {
-  res.send(`User ${req.params.id} deleted`);
+    res.send(`User ${
+        req.params.id
+    } deleted`);
 });
 
 app.listen(5000, () => {
-  console.log("Server on port 5000");
+    console.log("Server on port 5000");
 });
